de6f1af77621a1f9507e2eb45936407732c9e7a2
Bruna Fernandes <brunafernandes.idka@gmail.com>
2024-04-22 19:29:30 -0300

    feat: normalização transação

--- src/fetchData.ts
+++ src/fetchData.ts
@@ -1,9 +1,7 @@
 export default async function fetchData<T>(url: string): Promise<T | null> {
   try {
     const response = await fetch(url);
-    console.log(response);
     if (!response.ok) throw new Error("Erro: " + response.status);
-
     const json = await response.json();
     return json;
   } catch (error) {
--- src/script.ts
+++ src/script.ts
@@ -1,33 +1,13 @@
 import fetchData from "./fetchData.js";
-
-type TransacaoPagamento = "Boleto" | "Cartão de Crédito";
-type TransacaoStatus =
-  | "Paga"
-  | "Recusada pela operadora de cartão"
-  | "Aguardando Pagamento"
-  | "Estornada";
-
-interface TransacaoAPI {
-  Nome: string;
-  ID: number;
-  Date: string;
-  Status: TransacaoStatus;
-  Email: string;
-  ["Valor (R$)"]: string;
-  ["Forma de Pagamento"]: TransacaoPagamento;
-  ["Cliente Novo"]: number;
-}
+import normalizarTransacao from "./normalizarTransacao.js";
 
 async function handleData() {
   const data = await fetchData<TransacaoAPI[]>(
     "https://api.origamid.dev/json/transacoes.json"
   );
-
-  if (data) {
-    data.forEach((item) => {
-      console.log(item["Valor (R$)"]);
-    });
-  }
+  if (!data) return;
+  const transacoes = data.map(normalizarTransacao);
+  console.log(transacoes);
 }
 
 handleData();
